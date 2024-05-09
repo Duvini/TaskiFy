2a3360dc2c5dec9838e16720b5b2cbd82d3fae29
Duvini Ranaweera <it22884510@my.sliit.lk>
2024-05-09 14:26:57 +0530

    Task Done

--- app/src/main/java/com/example/activity/TasksActivity.kt
+++ app/src/main/java/com/example/activity/TasksActivity.kt
@@ -1,12 +1,18 @@
 package com.example.activity
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
 import com.example.taskshabits.R
 
-class TasksActivity: AppCompatActivity()  {
+class TasksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tasks)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fcv_tasks)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/example/data/TasksRepository.kt
@@ -0,0 +1,21 @@
+package com.example.data
+
+import androidx.lifecycle.LiveData
+
+class TasksRepository(private val tasksDao: TasksDao) {
+
+    val readAllTasksData: LiveData<List<Tasks>> = tasksDao.readAllTasks()
+
+    fun addTaskData(tasks: Tasks){
+        tasksDao.addTask(tasks)
+    }
+
+    fun updateTaskData(tasks: Tasks){
+        tasksDao.updateTask(tasks)
+    }
+
+    fun deleteTaskData(tasks: Tasks){
+        tasksDao.deleteTask(tasks)
+    }
+
+}
