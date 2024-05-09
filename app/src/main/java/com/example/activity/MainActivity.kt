58ddcdd4af5088a182e77af0c42696f78c4d3efb
Duvini Ranaweera <it22884510@my.sliit.lk>
2024-05-09 12:47:56 +0530

    corrected

--- app/src/main/java/com/example/activity/MainActivity.kt
+++ app/src/main/java/com/example/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.taskshabits.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
